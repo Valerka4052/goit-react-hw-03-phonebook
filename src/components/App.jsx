069573df690c04efa5172f291744e447cc005095
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-03-05 09:46:27 +0200

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,69 @@
+import { Component } from "react";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  getStateValues = (data) => {
+    this.setState(({ contacts }) => {
+      return {
+        contacts: [data, ...contacts]
+      };
+    });
+  };
+ 
+  deleteItem = num => {
+    return this.setState(prevState => ({ contacts: prevState.contacts.filter(({ id }) => id !== num) }));
+  };
+
+  getFilter = (text) => {
+    this.setState({ filter: text });
+  };
+  
+  render() {
+    const { getStateValues, getFilter, deleteItem, state: { contacts, filter } } = this;
+    const filteredContacts = contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          textAlign: 'center',
+          color: '#010101'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        ><div>
+            <h1>Phonebook</h1>
+            <ContactForm
+              contacts={contacts}
+              getStateValues={getStateValues} />
+            <h2>Contacts</h2>
+            <Filter
+              getFilter={getFilter}
+            />
+          </div>
+          <ContactList
+            filteredContacts={filteredContacts}
+            deleteItem={deleteItem} />
+        </div>
+      </div>
+    );
+  };
+};
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,37 @@
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { List } from './ContactList.styled';
+
+
+export class ContactList extends Component {
+    static propTypes = {
+        deleteItem: PropTypes.func.isRequired,
+        filteredContacts: PropTypes.arrayOf(
+            PropTypes.exact({
+                name: PropTypes.string.isRequired,
+                number: PropTypes.string.isRequired,
+                id: PropTypes.string.isRequired,
+            }).isRequired,
+        ).isRequired,
+    };
+
+    render() {
+        const { filteredContacts, deleteItem } = this.props;
+         console.log(filteredContacts)
+        return (
+            <List>
+                {filteredContacts.map(({ name, number, id }) => {
+                    return <ContactListItem key={id}
+                        name={name}
+                        number={number}
+                        id={id}
+                        deleteItem={deleteItem}
+                    />
+                }
+                )}
+            </List>
+        );
+    };
+};
+
